6a4a872596e9feb72d8c1366de664c8201727ab1
Azhar Ahmed <ahmedazhar05@yahoo.com>
2022-06-03 23:29:28 +0530

    Major updates
    Looks like its working for any level/depth

--- yaml.py
+++ yaml.py
@@ -3,8 +3,10 @@ import re
 def getcleanline(stream):
 	line = stream.readline()
 	# matching lines that are not comments or unnecessary(empty) lines or partial lines that are not comments
-	mat = re.match(r'[^\s#].+?(?=\n|\s+#)', line)
+	mat = re.match(r' *(?!#)[^ \n].*?(?=\n| +#)', line)
 	if mat:
+		if not mat.group(0).endswith('\n'):
+			return mat.group(0) + '\n'
 		return mat.group(0)
 	elif line:
 		return getcleanline(stream)
@@ -27,33 +29,68 @@ def converttype(value):
 	else:
 		return value
 
-# determining the level/depth of the line and returning stripped line
-def relevel(line, tabs = '  '):
-	tab_count = len(tabs)
-	spaces_count = len(line) - len(line.lstrip(' '))
-	level = spaces_count // tab_count
-	line = line[level * tab_count:]
-	return level, line
-
-def parse(stream, next = None, level = 0, tab = None):
+def parse(stream, line = None, indent = ''):
 	if not line:
 		line = getcleanline(stream)
+	islist = False
 	keyname = None
-	obj = None
-
+	if re.match(r' *\- ', line):
+		islist = True
+		obj = []
+	else:
+		obj = {}
+	
 	while line:
-		lvl, line = relevel(line, tab)
-		if lvl == level - 1:
-			return obj, line
+		if not indent == '':
+			if line.startswith(indent):
+				line = line[len(indent):]
+			else:
+				return obj, line
+
+		if line.startswith(' '):
+			o, line = parse(stream, indent + line, indent + (' ' * (len(line) - len(line.lstrip()))))
+			if keyname:
+				obj[keyname] = o
+			elif islist:
+				obj.append(o)
+			else:
+				raise Exception("Key-Value pair cannot be determined")
+			
+			if not line or not indent + line.lstrip() == line:
+				return obj, line
+			else:
+				continue
+				# line = line[len(indent):]
 		
 		if line.startswith('- '):
 			line = line[2:]
-			islistitem = True
+			if not islist:
+				if keyname:
+					if obj[keyname] == None:
+						o, line = parse(stream, '- ' + line, indent)
+						obj[keyname] = o
 
+					if not line or not indent + line.lstrip() == line:
+						return obj, line
+					else:
+						continue
+				else:
+					raise Exception("Key-Value pair cannot be determined")
+		elif islist:
+			return obj, line
+
+		# finding key-value separator `:`
 		sep = re.search(r':(?= +[^\n]| *\n)', line)
-		if sep and obj == None:
-			obj = {}
-		if sep and type(obj) == dict:
+
+		if sep and islist:
+			o, line = parse(stream, '  ' + indent + line, indent + (' ' * (len(line) - len(line.lstrip(' ')) + 2)))
+			obj.append(o)
+			if not line or not indent + line.lstrip() == line:
+				return obj, line
+			else:
+				continue
+
+		if sep:
 			keyname = line[:sep.start(0)].strip()
 			if keyname[0] == keyname[-1] and keyname[0] in ['"', "'"]:
 				keyname = keyname[1:-1]
@@ -61,18 +98,20 @@ def parse(stream, next = None, level = 0, tab = None):
 			obj[keyname] = converttype(value)
 			if len(value) > 0:
 				keyname = None
-		elif islistitem:
-			l.append(converttype(line))
+		elif islist:
+			obj.append(converttype(line.strip()))
 		else:
-			raise Exception('Uneven spaces')
+			return obj, line
 		
 		line = getcleanline(stream)
+	
+	return obj, line
 
 
 import sys
 if __name__ == '__main__':
 	f = open(sys.argv[1].strip(), 'r')
-	
+
 	firstline = f.readline()
 	if firstline.strip() == '---':
 		print(parse(f)[0])
